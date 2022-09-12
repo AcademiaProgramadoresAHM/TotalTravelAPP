821397ce8cc220641762bd779f36f351609f43e5
Claudia1234567890 <109091065+Claudia1234567890@users.noreply.github.com>
2022-09-12 12:01:54 -0600

    selected en radiobutton :D

--- flutter_application_1/lib/Account_screen.dart
+++ flutter_application_1/lib/Account_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/rendering.dart';
 import 'package:flutter_application_1/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/hotel_booking/calendar_popup_view.dart';
@@ -18,7 +19,7 @@ class AccountInfo extends StatefulWidget {
 
 class _AccountInfo extends State<AccountInfo> with TickerProviderStateMixin {
   AnimationController? animationController;
-  var _sexo;
+  var _sexo = "M";
   List<HotelListData> hotelList = HotelListData.hotelList;
   final ScrollController _scrollController = ScrollController();
 
@@ -42,7 +43,6 @@ class _AccountInfo extends State<AccountInfo> with TickerProviderStateMixin {
     animationController?.dispose();
     super.dispose();
   }
-
   @override
   Widget build(BuildContext context) {
     return Theme(
@@ -176,18 +176,19 @@ class _AccountInfo extends State<AccountInfo> with TickerProviderStateMixin {
                                   isDense: true,
                                 ),
                               ),
-                            ),
+                            ), 
                             RadioListTile(
                                 title: Text("Masculino"),
                                 value: "M", 
                                 groupValue: _sexo, 
                                 onChanged: (value){
                                   setState(() {
+                                      softWrap: true;
                                       _sexo = value.toString();
                                   });
                                 },
                             ),
-                          RadioListTile(
+                            RadioListTile(
                                 title: Text("Femenino"),
                                 value: "F", 
                                 groupValue: _sexo, 
@@ -198,7 +199,8 @@ class _AccountInfo extends State<AccountInfo> with TickerProviderStateMixin {
                                   
                                 },
                             ),
-                          ]),
+                          ],
+                          ),
                         ),
                                   ],
                                 );
