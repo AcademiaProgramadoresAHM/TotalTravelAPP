6fa3dc1b726c57ae61e2558bde63e26c9bacfa67
RonaldIsai <87435489+RonaldIsai@users.noreply.github.com>
2022-09-29 21:41:56 -0600

    landing page finish

--- AHM_TOTAL_TRAVEL_APP/flutter_application_1/lib/Screens/LandingPage.dart
+++ AHM_TOTAL_TRAVEL_APP/flutter_application_1/lib/Screens/LandingPage.dart
@@ -5,6 +5,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:flutter_application_1/Components/Packages.dart';
 import 'package:intl/intl.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_application_1/hotel_booking/hotel_app_theme.dart';
 import 'package:flutter_application_1/hotel_booking/model/PlanModal.dart';
 import 'package:flutter_application_1/Screens/Login.dart';
@@ -44,105 +45,91 @@ class LandingPageState extends State<LandingPage> {
       );
       list.add(
         Container(
-          height: 600,
+          height: 650,
           width: context.width(),
-          child: PageView.builder(
-            controller: controller,
-            itemCount: PlanList.length,
-            onPageChanged: (index) {
-              pageIndex = index;
-              setState(() {});
-            },
-            itemBuilder: (_, int index) {
-              bool isPageIndex = selectedIndex == index;
-
-              return AnimatedContainer(
-                margin: EdgeInsets.symmetric(
-                    vertical: pageIndex == index ? 16 : 50, horizontal: 8),
-                height: pageIndex == index ? 0.5 : 0.45,
-                width: context.width(),
-                decoration: BoxDecoration(
-                  color: context.cardColor,
-                  borderRadius: BorderRadius.circular(16),
-                  boxShadow: defaultBoxShadow(),
-                ),
-                duration: 1000.milliseconds,
-                curve: Curves.linearToEaseOut,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Image.asset(
-                      planList[index].image.validate(),
-                      fit: BoxFit.cover,
-                      height: 190,
-                    ),
-                    SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          Text(PlanList[index].title.validate(),
-                              style: boldTextStyle(size: 30)),
-                          Text(PlanList[index].subTitle.validate(),
-                              style: secondaryTextStyle()),
-                          24.height,
-                          Text(PlanList[index].price.validate(),
-                              style: boldTextStyle(
-                                  size: 45, color: blueButtonAndTextColor)),
-                          Text(PlanList[index].planPriceSubTitle.validate(),
-                              style: secondaryTextStyle()),
-                          24.height,
-                          Container(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
-                            child: UL(
+          child: Container(
+            height: 650,
+            child: Container(
+              // margin: EdgeInsets.symmetric(
+              //     vertical: pageIndex == index ? 16 : 50, horizontal: 8),
+              // height: pageIndex == index ? 0.5 : 0.45,
+              height: 650,
+              width: context.width(),
+              decoration: BoxDecoration(
+                color: context.cardColor,
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: defaultBoxShadow(),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Image.asset(
+                    'assets/images/Argentina.jpg',
+                    fit: BoxFit.cover,
+                    height: 170,
+                  ),
+                  SizedBox(height: 20),
+                  SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Text(element['nombre'], style: boldTextStyle(size: 30)),
+                        Text(element['descripcion_Paquete'],
+                            style: secondaryTextStyle()),
+                        24.height,
+                        Text('\$0',
+                            style: boldTextStyle(
+                                size: 45, color: blueButtonAndTextColor)),
+                        Text('per user/month', style: secondaryTextStyle()),
+                        24.height,
+                        Container(
+                          height: 200,
+                          padding: EdgeInsets.symmetric(horizontal: 16),
+                          child: UL(
                               symbolType: SymbolType.Bullet,
                               symbolColor: Color.fromARGB(255, 101, 45, 144),
                               spacing: 24,
-                              children: List.generate(
-                                PlanList[index].optionList!.length,
-                                (i) => Text(
-                                    PlanList[index]
-                                        .optionList![i]
-                                        .title
-                                        .validate(),
-                                    style: boldTextStyle()),
-                              ),
-                            ),
+                              children: [
+                                Text(element['hotel'],
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w600)),
+                                Text(element['restaurante'],
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w600)),
+                                Text(element['duracion_Paquete'],
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w600)),
+                              ]),
+                        ),
+                      ],
+                    ),
+                  ).expand(),
+                  AppButton(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                          width: context.width() - 120,
+                          child: Text(
+                            'Adquirir paquete',
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Color.fromARGB(255, 80, 28, 111)),
                           ),
-                        ],
-                      ),
-                    ).expand(),
-                    AppButton(
-                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                      width: context.width() - 120,
-                      child: TextIcon(
-                        prefix: isPageIndex
-                            ? Icon(Icons.check,
-                                color: selectedIndex == index
-                                    ? Colors.green
-                                    : null,
-                                size: 16)
-                            : null,
-                        text: isPageIndex ? 'Adquirir' : 'Upgrade',
-                        textStyle: boldTextStyle(
-                            size: 18,
-                            color: isPageIndex ? Colors.green : white),
-                      ),
-                      onTap: () {
-                        setState(() {});
-                        selectedIndex = index;
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => Login()));
-                      },
-                      shapeBorder: RoundedRectangleBorder(
-                          borderRadius: radius(defaultRadius)),
-                      color: isPageIndex
-                          ? Colors.green.shade100
-                          : blueButtonAndTextColor,
-                    ).paddingBottom(16),
-                  ],
-                ),
-              );
-            },
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Login()));
+                          },
+                          shapeBorder: RoundedRectangleBorder(
+                              borderRadius: radius(defaultRadius)),
+                          color: Color.fromARGB(255, 249, 233, 255))
+                      .paddingBottom(16),
+                ],
+              ),
+            ),
           ),
         ),
       );
@@ -217,22 +204,25 @@ class LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: context.scaffoldBackgroundColor,
-      body: Container(
+      body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
             FutureBuilder<dynamic>(
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  return Wrap(
-                      spacing: 8,
-                      runSpacing: 4,
-                      alignment: WrapAlignment.start,
-                      crossAxisAlignment: WrapCrossAlignment.start,
-                      direction: Axis.horizontal,
-                      runAlignment: WrapAlignment.start,
-                      verticalDirection: VerticalDirection.down,
-                      clipBehavior: Clip.none,
-                      children: LandScreenPackage(snapshot.data, context));
+                  return Container(
+                    height: 650,
+                    child: CarouselSlider(
+                      options: CarouselOptions(
+                        height: 550,
+                        // aspectRatio: 1.0,
+                        enlargeCenterPage: true,
+                        scrollDirection: Axis.horizontal,
+                        autoPlay: true,
+                      ),
+                      items: LandScreenPackage(snapshot.data, context),
+                    ),
+                  );
                 } else {
                   return Text("No data");
                 }
