8ca2603716d631cefe06403b49a793da23e85bdb
Choomlee <109097735+Choomlee@users.noreply.github.com>
2022-10-26 07:43:46 -0600

    A deja de besar el ventilador

---
+++ AHM_TOTAL_TRAVEL_APP/flutter_application_1/lib/Components/ShoppingHistory.dart
@@ -0,0 +1,175 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'Decodificador.dart';
+import 'package:http/http.dart' as http;
+
+Future<dynamic> GetListadoShoppingHistory() async {
+  String url_list =
+      "https://totaltravelapi.azurewebsites.net/API/RecordPayment/List";
+
+  final response = await http.get(Uri.parse(url_list));
+  if (response.statusCode == 200) {
+    Map<String, dynamic> userMap = jsonDecode(response.body);
+    var user = Decodificador.fromJson(userMap);
+    return user.data;
+  } else {
+    print("Error " + response.statusCode.toString());
+  }
+}
+
+List<Padding> ListDefaultShoppingHistory(
+    List<dynamic> data, BuildContext context) {
+  List<Padding> list = [];
+  final _controller = PageController();
+
+  data.forEach((element) {
+    list.add(Padding(
+      padding: EdgeInsetsDirectional.fromSTEB(16, 8, 16, 4),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 4,
+              color: Color(0x32000000),
+              offset: Offset(0, 2),
+            )
+          ],
+        ),
+        child: Column(
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    height: 0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(0),
+                        bottomRight: Radius.circular(0),
+                        topLeft: Radius.circular(12),
+                        topRight: Radius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(10, 10, 16, 12),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Container(
+                        width: 4,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(101, 45, 143, 1),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 6,
+                        child: Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    element['descripcion_Paquete'].toString(),
+                                    style: TextStyle(
+                                      fontFamily: 'Outfit',
+                                      color: Color(0xFF090F13),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  Text(
+                                    '\$' + element['montoPago'].toString(),
+                                    style: TextStyle(
+                                      fontFamily: 'Outfit',
+                                      color: Color.fromRGBO(101, 45, 143, 1),
+                                      fontSize: 19,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                element['nombreCompleto'],
+                                style: TextStyle(
+                                  fontFamily: 'Outfit',
+                                  color: Color.fromRGBO(101, 45, 143, 1),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                element['dni'],
+                                style: TextStyle(
+                                  fontFamily: 'Outfit',
+                                  color: Color.fromRGBO(101, 45, 143, 1),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                element['fechaPago'],
+                                style: TextStyle(
+                                  fontFamily: 'Outfit',
+                                  color: Color.fromRGBO(101, 45, 143, 1),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                element['tipoPago'],
+                                style: TextStyle(
+                                  fontFamily: 'Outfit',
+                                  color: Color.fromRGBO(101, 45, 143, 1),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsetsDirectional.fromSTEB(
+                                          2, 12, 24, 12),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
+  });
+
+  return list;
+}
