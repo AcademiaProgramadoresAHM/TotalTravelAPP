a2aae1cadecf1de1b6560259dcff5ffeb983e9e7
AngelGuillen02 <109125563+AngelGuillen02@users.noreply.github.com>
2022-10-26 01:17:22 -0600

    Loscuentosdelpeluca

--- AHM_TOTAL_TRAVEL_APP/flutter_application_1/lib/Models/ReservationViewModel.dart
+++ AHM_TOTAL_TRAVEL_APP/flutter_application_1/lib/Models/ReservationViewModel.dart
@@ -6,12 +6,16 @@ class ReservationUserViewModel {
   int? CantidadPagos;
   int? Id_Paquete;
   String? DescripcionPaquete;
+  String? DurecionPaquete;
   double? precio;
   int? ReservacionHotelID;
   DateTime? Fecha_Entrada;
   DateTime? Fecha_Salida;
   int? Hotel_ID;
   String? Nombre_Hotel;
+  String? Habitacion;
+  String? Categoria_Habitacion;
+  int? Camas;
   int? Id_Cliente;
   String? Nombre;
   String? Apellido;
@@ -31,12 +35,16 @@ class ReservationUserViewModel {
         CantidadPagos = Json['cantidadPagos'],
         Id_Paquete = Json['id_Paquete'],
         DescripcionPaquete = Json['descripcionPaquete'],
+        DurecionPaquete = Json['durecionPaquete'],
         precio = Json['precio'],
         ReservacionHotelID = Json['reservacionHotelID'],
         Fecha_Entrada = Json['fecha_Entrada'],
         Fecha_Salida = Json['fecha_Salida'],
         Hotel_ID = Json['hotel_ID'],
         Nombre_Hotel = Json['nombre_Hotel'],
+        Habitacion = Json['habitacion'],
+        Categoria_Habitacion = Json['Categoria_Habitacion'],
+        Camas = Json['Camas'],
         Id_Cliente = Json['id_Cliente'],
         Nombre = Json['nombre'],
         Apellido = Json['apellido'],
